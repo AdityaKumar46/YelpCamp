42540eddd74e7a91fb32f6e20e5f941a3deec6e8
Aditya Kumar <adityakuumar466@gmail.com>
2021-05-24 18:19:40 +0530

    make it better

--- app.js
+++ app.js
@@ -50,9 +50,22 @@ db.once('open', function() {
 
 const app = express();
 
-app.engine('ejs', ejsMate);
+// Require static assets from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set 'views' directory for any views 
+// being rendered res.render()
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+/** app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+*/
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
